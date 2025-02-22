2a4f3ce10647b291902b712267bbdf2ca63f5cc3
ariaubrot <ariaputrapratamaaa@gmail.com>
2025-02-23 02:27:47 +0700

    Update redis.ts

--- src/redis.ts
+++ src/redis.ts
@@ -1,41 +1,22 @@
-import Redis from "ioredis";
-import { ApiResponse } from "./types/anime";
+import mongoose from "mongoose";
 
-const getRedisConnectionURL = (): string => {
-  if (process.env.NODE_ENV === "development") return "";
-  if (process.env.REDIS_URL) {
-    return process.env.REDIS_URL;
-  }
+export async function connect() {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017", {
+            dbName: "Animetrix",
+        });
+        const connection = mongoose.connection;
+        connection.setMaxListeners(20);
+        connection.on("connected", () => {
+            console.log("MongoDB connected successfully");
+        });
 
-  throw new Error("[REDIS]: Redis Connection URL is must!");
-};
-
-export const redis = new Redis(getRedisConnectionURL());
-
-export async function storeOrGetFromRedis<T>(
-  key: string,
-  fetch: () => Promise<T>,
-  tts = 3600,
-) {
-  if (!key) {
-    throw new Error("[Key] is must!");
-  }
-  try {
-    const cachedData = await redis.get(key);
-
-    if (cachedData) {
-      console.log("[Redis]: Cached from redis DB");
-      return JSON.parse(cachedData) as ApiResponse<T>;
+        connection.on("error", (err) => {
+            console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+            process.exit();
+        });
+    } catch (error) {
+        console.log("Something went wrong!");
+        console.log(error);
     }
-
-    console.log("[API]: Fetched from API!");
-    const response = await fetch();
-
-    await redis.set(key, JSON.stringify(response), "EX", tts);
-
-    console.log("[API]: Stored on Redis!");
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
 }
